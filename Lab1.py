37c7ff2e435451a2866b57af9298b6615787fded
Artem Yushkov <artem.yushkov2012@mail.ru>
2023-09-12 16:22:00 +0700

    remove Lab1.py

--- Lab1.py
+++
@@ -1,9 +0,0 @@
-import openpyxl
-import Pandas
-
-workbook = openpyxl.load_workbook('lab_pi_101.xlsx')
-
-worksheet = workbook.active ('Лист1') 
-
-for row in worksheet.iter_rows(values_only=True):
-    print(row)
