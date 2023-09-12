fad66035d6901a928d97507253ec272598e28b09
Artem Yushkov <artem.yushkov2012@mail.ru>
2023-09-12 22:03:31 +0700

    Итоговый файл Lab1

---
+++ Lab1.py
@@ -0,0 +1,36 @@
+import openpyxl
+import pandas as pd
+
+workbook = openpyxl.load_workbook('lab_pi_101.xlsx') #открытие через openpyxl
+worksheet = workbook['Лист1']
+
+data = []
+
+for row in worksheet.iter_rows(values_only=True): #перенос таблицы в массив
+    data.append(row)
+
+df = pd.DataFrame(data) 
+df.columns = df.iloc[0]
+df = df.iloc[1:]
+
+pi101_row = df[df['Группа']=='ПИ101']
+#студенты с уникальным номером из группы Пи101 
+unique_student_row = pi101_row['Личный номер студента'].unique()  
+unique_student_row_str = [str(number) for number in unique_student_row] 
+unique_student_row_str = ', '.join(unique_student_row_str)
+#годы контроля
+year_control = pi101_row['Год'].unique()
+year_control_row_str = [str(number) for number in year_control]
+year_control_row_str = ', '.join(year_control_row_str)
+#тип контроля
+type_control = pi101_row['Уровень контроля'].unique()
+type_control_str = [str(number) for number in type_control]
+type_control_str = ', '.join(type_control_str)
+
+#global print
+print('В исходном датасете содержалось', len(df) ,'оценок, из них', len(pi101_row),
+'оценок относятся к группе ПИ101\n'
+'В датасете находятся оценки', len(unique_student_row) ,
+'студентов со следующими личными номерами:', unique_student_row_str, 
+'\nИспользуемые формы контроля:', type_control_str ,
+'\nДанные представлены по следующим учебным годам:',year_control_row_str)
